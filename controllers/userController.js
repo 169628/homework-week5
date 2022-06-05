e1611ea015157add25701bbd061909273353fece
169628 <rainbow.pipi.1986@outlook.com>
2022-06-06 02:40:44 +0800

    delete noused

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,69 +37,8 @@ const signUp = async (req, res, next) => {
     sendJWT(newUser,res);
 }
 
-//登入
-const signIn = async (req, res, next) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return next(err.appError( 400,'帳號密碼不可為空',next));
-    }
-    if(!validator.isLength(password,{min:6})){
-        return next(err.appError(400,'密碼字數不可低於 6 碼',next));
-    }
-    const user = await User.findOne({ mail: email }).select('+password');
-    if(user === null){
-        return next(err.appError(400,'無此使用者,請註冊',next));
-    }
-    const auth = await bcrypt.compare(password, user.password);
-    if(!auth){
-        return next(err.appError(400,'您的密碼不正確',next));
-    }
-    sendJWT(user,res);
-}
-
-//修改密碼
-const updatePassword = async (req, res, next) => {
-    const {password,confirmPassword } = req.body;
-    if(password!==confirmPassword){
-        return next(err.appError("400","密碼不一致！",next));
-    }
-    if(!validator.isLength(password,{min:6})){
-        return next(err.appError(400,'密碼字數不可低於 6 碼',next));
-    }
-    newPassword = await bcrypt.hash(password,12);
-    const user = await User.findByIdAndUpdate(req.user.id,{
-        password:newPassword
-    });
-    sendJWT(user,res);
-}
-
-//取得個人資料
-const profile = async (req, res, next) => {
-    response(res,req.user)
-}
-
-//更新個人資料
-const updateProfile = async (req, res, next) => {
-    let { photo, name, sex } = req.body;
-    if(!photo && !name && !sex){
-        return next(err.appError(400,"您沒有更新任何資料",next));
-    }else{
-        await User.findByIdAndUpdate(req.user._id,{
-            photo,
-            name,
-            sex
-        })
-        const newProfile = await User.findById(req.user._id);
-        response(res,newProfile);
-    }
-}
-
 
 
 module.exports = {
     signUp,
-    signIn,
-    updatePassword,
-    profile,
-    updateProfile
 };
--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,6 @@ const isAuth = require('../middleware/isMember');
 
 //註冊
 router.post('/sign_up',err.handleErrorAsync(usercontroller.signUp));
-//登入
-router.post('/sign_in',err.handleErrorAsync(usercontroller.signIn));
-//修改密碼
-router.post('/updatePassword',isAuth,err.handleErrorAsync(usercontroller.updatePassword));
-//取得個人資料
-router.get('/profile',isAuth,err.handleErrorAsync(usercontroller.profile));
-//更新個人資料
-router.post('/profile',isAuth,err.handleErrorAsync(usercontroller.updateProfile));
 
 
 module.exports = router;
